0dd3f8024e3ec935367f179aa0409410403b0b0b
Roman Rasskazov <kajifat10@gmail.com>
2018-07-03 13:47:55 +0300

    =navbar, slider

---
+++ src/App.vue
@@ -0,0 +1,94 @@
+<template>
+  <div id="app">
+    <Navbar></Navbar>
+    <div class=container>
+      <slick
+        ref="slick"
+        :options="slickOptions"
+      >
+        <div v-for="(img, key) in images" :key="key">
+          <a :href="img.link"><img :src="img.src" alt=""></a>
+        </div>
+      </slick>
+    </div>
+    <div class="test-header"></div>
+  </div>
+</template>
+
+<script>
+import Navbar from './components/Navbar'
+import Slick from 'vue-slick'
+
+export default {
+  name: "app",
+  components: {Navbar, Slick},
+  data() {
+    return {
+      images: [
+        {
+          link: "http://placehold.it/300x100",
+          src: "http://placehold.it/300x100"
+        },
+        {
+          link: "http://placehold.it/300x100",
+          src: "http://placehold.it/300x100"
+        },
+        {
+          link: "http://placehold.it/300x100",
+          src: "http://placehold.it/300x100"
+        },
+        {
+          link: "http://placehold.it/300x100",
+          src: "http://placehold.it/300x100"
+        },
+        {
+          link: "http://placehold.it/300x100",
+          src: "http://placehold.it/300x100"
+        },
+        {
+          link: "http://placehold.it/300x100",
+          src: "http://placehold.it/300x100"
+        },
+        {
+          link: "http://placehold.it/300x100",
+          src: "http://placehold.it/300x100"
+        }
+      ],
+      slickOptions: {
+        arrows: false,
+        slidesToShow: 3,
+        responsive: [
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 1
+            }
+          }
+        ]
+      }
+    }
+  }
+}
+</script>
+
+<style>
+@import '../node_modules/slick-carousel/slick/slick.css';
+
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  display: flex;
+  justify-content: center;
+}
+
+.container {
+  padding-top: 70px;
+  max-width: 920px;
+}
+
+.test-header {
+  height: 1000px;
+}
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,61 @@
+<template>
+  <nav class="navigation">
+      <div class="navigation__inner">
+        <ul class="navigation__list">
+          <li class="navigation__item"><a class="navigation__link" href="#">Home</a></li>
+          <li class="navigation__item"><a class="navigation__link" href="#">About Us</a></li>
+          <li class="navigation__item"><a class="navigation__link" href="#">Contact Us</a></li>
+        </ul>
+      </div>
+    </nav>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+  data () {
+    return {}
+  }
+}
+</script>
+
+<style>
+.navigation {
+  width: 100%;
+  max-width: 920px;
+  position: fixed;
+  border-bottom: 1px solid #ccc;
+  background: #fff;
+  height: 50px;
+  z-index: 1;
+}
+.navigation__inner {
+  display: flex;
+  height: 50px;
+  margin: 0 auto;
+  overflow: hidden;
+}
+
+.navigation__link {
+  display: block;
+  padding-right: 40px;
+  font-size: 14px;
+  line-height: 50px;
+  text-decoration: none;
+  color: #000;
+  cursor: pointer;
+}
+.navigation__link:hover {
+  text-decoration: underline;
+}
+
+.navigation__list {
+  display: flex;
+  flex-wrap: wrap;
+  margin: 0;
+  padding: 0;
+  list-style: none;
+  background: #fff;
+}
+</style>
+
